c15d18a22316011bd8d356d0cb17ebad046c54d9
CANTIER\bharat <CANTIER\bharat>
2024-12-19 15:53:59 +0530

    textfiled changes

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,71 +1,77 @@
-import React, { FC } from 'react'
-import { TextField as MuiTextField, Stack, TextFieldProps } from "@mui/material"
-
+import React, { FC } from "react";
+import {
+  TextField as MuiTextField,
+  Stack,
+  TextFieldProps,
+} from "@mui/material";
 
 type ITextFieldProps = TextFieldProps & {
-    width?: string | number | undefined
+  width?: string | number | undefined;
 };
 
 const TextField: FC<ITextFieldProps> = (props: ITextFieldProps) => {
-    return (
-        <MuiTextField {...props}
-            variant="outlined"
-            slotProps={{
-                inputLabel: {
-                    shrink: true,
-                    style: {
-                        visibility: "visible",
-                        position: 'absolute',
-                        top: '-15px',
-                        fontWeight: 700,
-                        fontSize: 20
-                    },
-                },
+  return (
+    <MuiTextField
+      {...props}
+      variant="outlined"
+      slotProps={{
+        inputLabel: {
+          shrink: true,
+          style: {
+            visibility: "visible",
+            position: "absolute",
+            top: "-15px",
+            fontWeight: 700,
+            fontSize: 20,
+          },
+        },
+      }}
+      sx={{
+        borderRadius: "5px",
+        width: props.width ?? "20vw",
+        ".MuiOutlinedInput-notchedOutline": {
+          border: "unset",
+        },
+        "& .MuiInputBase-root .MuiOutlinedInput-root": {
+          borderRadius: "15px",
+          px: 1,
+        },
+        "& .MuiFormLabel-asterisk": {
+          color: "red",
+        },
+        "& .MuiInputBase-root": {
+          backgroundColor: "#F7F7F7",
+          p: 0,
+          height: "50px",
+        },
+        "& .MuiInputBase-input": {
+          fontSize: 18,
+          color: "#454545",
+          textTransform: "capitalize",
+        },
+        "& .MuiInputLabel-root": {
+          color: "#454545", // Label color
+        },
+        "& .MuiInputLabel-root.Mui-focused": {
+          color: "#002856", // Label color when focused
+        },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: "#00476d", // Default border color
+          },
+          "&:hover fieldset": {
+            borderColor: "#002856", // Border color on hover
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: "#002856", // Border color when focused
+          },
+        },
+        "& .MuiFormHelperText-root": {
+            ml: "4px",
+        },
+      }}
+    />
+  );
+};
 
-            }}
-            sx={{
-                borderRadius: "5px",
-                width: props.width ?? "20vw",
-                backgroundColor: "#F7F7F7",
-                ".MuiOutlinedInput-notchedOutline": {
-                    border: "unset",
-                },
-                "& .MuiInputBase-root .MuiOutlinedInput-root": {
-                    borderRadius: "15px",
-                    px: 1,
-                },
-                "& .MuiFormLabel-asterisk": {
-                    color: 'red'
-                },
-                "& .MuiInputBase-root": {
-                    p: 0,
-                    height: "50px",
-                },
-                "& .MuiInputBase-input": {
-                    fontSize: 18,
-                    color: "#454545",
-                    textTransform: "capitalize",
-                },
-                "& .MuiInputLabel-root": {
-                    color: "#454545", // Label color
-                },
-                "& .MuiInputLabel-root.Mui-focused": {
-                    color: "#002856", // Label color when focused
-                },
-                "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                        borderColor: "#00476d", // Default border color
-                    },
-                    "&:hover fieldset": {
-                        borderColor: "#002856", // Border color on hover
-                    },
-                    "&.Mui-focused fieldset": {
-                        borderColor: "#002856", // Border color when focused
-                    },
-                },
-            }}
-        />
-    )
-}
-
-export default TextField
+export default TextField;
