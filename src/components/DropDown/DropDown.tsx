08e5e5fce214e6ac60002bc7d2b28f49abde5e92
CANTIER\bharat <CANTIER\bharat>
2024-12-23 12:09:19 +0530

    fixing exports

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -7,73 +7,74 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 // interfaces
 type menus = {
-    value: string | number;
-    label: string;
+  value: string | number;
+  label: string;
 };
 interface IDropDown {
-    label: string;
-    value: any;
-    onChange: (event: SelectChangeEvent) => void;
-    menus: menus[];
-    fullWidth?: boolean;
-    height?: string | number;
+  label: string;
+  value: any;
+  onChange: (event: SelectChangeEvent) => void;
+  menus: menus[];
+  fullWidth?: boolean;
+  height?: string | number;
 }
 // type guards
 function isMenus(obj: menus[]): obj is menus[] {
-    function isMenu(obj: any): obj is menus {
-        return (
-            typeof obj === "object" &&
-            obj !== null &&
-            (typeof obj.value === "string" || typeof obj.value === "number") &&
-            typeof obj.label === "string"
-        );
-    }
+  function isMenu(obj: any): obj is menus {
+    return (
+      typeof obj === "object" &&
+      obj !== null &&
+      (typeof obj.value === "string" || typeof obj.value === "number") &&
+      typeof obj.label === "string"
+    );
+  }
 
-    return Array.isArray(obj) && obj.every((menu: any) => isMenu(menu));
+  return Array.isArray(obj) && obj.every((menu: any) => isMenu(menu));
 }
 
 const DropDown: React.FC<IDropDown> = ({
-    onChange,
-    label,
-    value,
-    menus,
-    fullWidth,
-    height,
+  onChange,
+  label,
+  value,
+  menus,
+  fullWidth,
+  height,
 }) => {
-    return (
-        <Box sx={{ minWidth: !fullWidth ? 120 : "100%" }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                    {label || "No Label"}
-                </InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={value || ""}
-                    label={label || "No Label"}
-                    onChange={onChange}
-                    sx={{
-                        height: height ? height : "48px" || "48px", // Adjust the height of the Select input
-                        "& .MuiSelect-select": {
-                            display: "flex",
-                            alignItems: "center",
-                        },
-                    }}
-                >
-                    {menus?.length > 0 && isMenus(menus) ? (
-                        menus?.map((item, index) => (
-                            <MenuItem key={item.label + index} value={item.value}>
-                                {item.label}
-                            </MenuItem>
-                        ))
-                    ) : (
-                        <MenuItem key={0} value={0}>
-                            No Menu
-                        </MenuItem>
-                    )}
-                </Select>
-            </FormControl>
-        </Box>
-    );
+  return (
+    <Box sx={{ minWidth: !fullWidth ? 120 : "100%" }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">
+          {label || "No Label"}
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value || ""}
+          label={label || "No Label"}
+          onChange={onChange}
+          sx={{
+            height: height ? height : "48px" || "48px", // Adjust the height of the Select input
+            "& .MuiSelect-select": {
+              display: "flex",
+              alignItems: "center",
+            },
+          }}
+        >
+          {menus?.length > 0 && isMenus(menus) ? (
+            menus?.map((item, index) => (
+              <MenuItem key={item.label + index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))
+          ) : (
+            <MenuItem key={0} value={0}>
+              No Menu
+            </MenuItem>
+          )}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 };
+
 export default React.memo(DropDown);
